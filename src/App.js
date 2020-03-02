a136d1d3580785391b958062bcbe6d22a1e9e084
David <51271545+davidthemavid@users.noreply.github.com>
2020-03-02 15:18:16 -0800

    add note date to note component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Note from "./components/Note";
 import styles from "./styles/styles.css";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Note />
       <Footer />
     </div>
   );
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,5 +1,16 @@
 import React from "react";
 
+const noteDay = new Date().getDay() + 1;
+const noteMonth = new Date().getMonth() + 1;
+
 export default function Note() {
-  return <div>test</div>;
+  return (
+    <div className="note">
+      <h1 className="note__title">Note Title</h1>
+      <p className="noteDate">
+        {noteDay}/{noteMonth}
+      </p>
+      <p>content</p>
+    </div>
+  );
 }
