f667607354cec5c27b5a4cc6f9efa914122055b0
David <51271545+davidthemavid@users.noreply.github.com>
2020-03-09 15:31:18 -0700

    map note data to card component

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -4,17 +4,21 @@ const getDate = () => {
   const noteDay = new Date().getDay() + 1;
   const noteMonth = new Date().getMonth() + 1;
   const date = noteDay + "/" + noteMonth;
+  console.log(noteDay);
   return date;
 };
 
 let date = getDate();
+console.log(date);
 
-export default function Note() {
+export default function Note(props) {
+  //console.log(props);
+
   return (
     <div className="note">
-      <h1 className="note__title">Note Title</h1>
+      <h1 className="note__title">{props.title}</h1>
       <p className="noteDate">{date}</p>
-      <p>content</p>
+      <p>{props.content}</p>
     </div>
   );
 }
